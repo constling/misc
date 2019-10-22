4b97afcfe1668978dbf1c82e52a98473fa3b65d8
lingxing.lx <lingxing.lx@alibaba-inc.com>
2019-10-22 17:22:56 +0800

    444

---
+++ MyBatis/src/main/java/com/yisui/App.java
@@ -0,0 +1,13 @@
+package com.yisui;
+
+public class App {
+    public static void main(String[] arsgs) {
+        System.out.println("app begin");
+
+        // 指定全局配置文件
+        String resource = "classpath:mybatis-config.xml";
+
+        // 读取配置文件
+
+    }
+}
